e13777055df4b007ce4247054ea19a0ef4c8abc6
Jahid Uddin <jahiduddin922@gmail.com>
2022-08-09 20:47:01 +0600

    Add Project

---
+++ BankAccount-Prosess/BankAccount/DataContext/BankApplictionContext.cs
@@ -0,0 +1,14 @@
+﻿using BankAccount.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankAccount.DataContext
+{
+    public class BankApplictionContext : DbContext
+    {
+        public BankApplictionContext(DbContextOptions<BankApplictionContext>options) : base(options)
+        {
+                
+        }
+        public DbSet<PaymentDetail> PaymentDetail { get; set; }
+    }
+}
---
+++ BankAccount-Prosess/BankAccount/Migrations/20220809135543_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BankAccount.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PaymentDetail",
+                columns: table => new
+                {
+                    PaymentDetailId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CardOwnerName = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    CardNumber = table.Column<string>(type: "nvarchar(16)", nullable: true),
+                    ExpirationDate = table.Column<string>(type: "nvarchar(6)", nullable: true),
+                    SecurityCode = table.Column<string>(type: "nvarchar(6)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentDetail", x => x.PaymentDetailId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PaymentDetail");
+        }
+    }
+}
